502f70db4e70cddbfc061c73648d8236136349c5
Matthew He <matthew.he.au@gmail.com>
2020-06-02 18:01:59 +1000

    while for loop

--- demo2.js
+++ demo2.js
@@ -1,6 +1,12 @@
-const smartness = 450;
-if (smartness > 500) {
-    console.log('You suceeded');
-} else {
-    console.log('You failed');
-};
+// const smartness = 450;
+// if (smartness > 500) {
+//     console.log('You suceeded');
+// } else {
+//     console.log('You failed');
+// };
+var balance = 16;
+var beerPrice = 5;
+while (balance >= beerPrice) {
+    balance = balance - 5
+    console.log("time to go home")
+}
