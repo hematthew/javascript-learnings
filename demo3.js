01628b357b087957c1d0fe8d2bb13ba4775b2df9
Matthew He <matthew.he.au@gmail.com>
2020-06-12 18:27:44 +1000

    While Beer Example

--- demo3.js
+++ demo3.js
@@ -1,1 +1,8 @@
-console.log('I need to eat lunch!');
+var balance = 15;
+var beerPrice = 5;
+while (balance >= beerPrice) {
+    console.log('You are drunk!');
+    balance = balance - beerPrice;
+
+}
+console.log('Go home');
