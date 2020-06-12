884c0d844f9d3eb5aaa6ba2b3d16537741b10fa0
Matthew He <matthew.he.au@gmail.com>
2020-06-12 18:52:28 +1000

    While Number Example 1-20

--- demo3.js
+++ demo3.js
@@ -1,8 +1,13 @@
-var balance = 15;
-var beerPrice = 5;
-while (balance >= beerPrice) {
-    console.log('You are drunk!');
-    balance = balance - beerPrice;
+// var balance = 15;
+// var beerPrice = 5;
+// while (balance >= beerPrice) {
+//     console.log('You are drunk!');
+//     balance = balance - beerPrice;
 
+// }
+// console.log('Go home');
+var number = 1;
+while (number <= 20) {
+    console.log(number);
+    number = number + 1;
 }
-console.log('Go home');
