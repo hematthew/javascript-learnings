3f1df1305afda0c2d7df3ae51b5b937fc16d14a4
Matthew He <matthew.he.au@gmail.com>
2020-06-12 19:09:14 +1000

    For Number Example

--- demo3.js
+++ demo3.js
@@ -1,13 +1,17 @@
-// var balance = 15;
-// var beerPrice = 5;
-// while (balance >= beerPrice) {
-//     console.log('You are drunk!');
-//     balance = balance - beerPrice;
+// // var balance = 15;
+// // var beerPrice = 5;
+// // while (balance >= beerPrice) {
+// //     console.log('You are drunk!');
+// //     balance = balance - beerPrice;
 
+// // }
+// // console.log('Go home');
+// var number = 1;
+// while (number <= 20) {
+//     console.log(number);
+//     number = number + 1;
 // }
-// console.log('Go home');
-var number = 1;
-while (number <= 20) {
-    console.log(number);
-    number = number + 1;
+for (let donaldTrump = 50; donaldTrump >= 20;
+    donaldTrump--) {
+    console.log(donaldTrump);
 }
