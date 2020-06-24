3fa6dd81565e4a1e334e28f95b05928f74053503
Matthew He <matthew.he.au@gmail.com>
2020-06-24 20:03:21 +1000

    Skip Counting by 2 for

--- demo1.js
+++ demo1.js
@@ -1,5 +1,10 @@
-for (let index = 1; index <= 100; index++) {
-    if (index % 5 == 1) {
-        console.log(index);
+// for (let index = 1; index <= 100; index++) {
+//     if (index % 5 == 1) {
+//         console.log(index);
+//     }
+// }
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
     }
 }
