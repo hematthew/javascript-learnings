a54b56bf3021efa57ac741ea8098acdde446b612
Matthew He <matthew.he.au@gmail.com>
2020-07-02 15:08:13 +1000

    Array Fruits print out watermelon

---
+++ DATA-STUCTURES/array.js
@@ -0,0 +1,7 @@
+// var fruit = ['mango', 'apple', 'lychee', 'watermelon'];
+
+// console.log(fruit[2]);
+
+var fruits = ['mango', 'apple', ['lychee', 'watermelon'], 'lemon'];
+
+console.log(fruits[2][1]);
