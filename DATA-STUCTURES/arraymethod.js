abf7587ef1a888a3fc9a666fa047a6dcf7db0c31
Matthew He <matthew.he.au@gmail.com>
2020-07-02 15:45:39 +1000

    Object Prints I love eating sugar

--- DATA-STUCTURES/arraymethod.js
+++ DATA-STUCTURES/arraymethod.js
@@ -3,4 +3,5 @@ var recipe = {
     people: 2,
     ingredients: ['flour', 'eggs', 'milk', 'sugar']
 }
-console.log(recipe.ingredients[1]);
+// console.log(recipe.ingredients[1]);
+console.log('I love eating ' + recipe.ingredients[3]);
