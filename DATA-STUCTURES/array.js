e724248eba8a02dda32b163b073eb8c234fca5ed
Matthew He <matthew.he.au@gmail.com>
2020-07-02 15:15:09 +1000

    Array Items prints out 0

--- DATA-STUCTURES/array.js
+++ DATA-STUCTURES/array.js
@@ -2,6 +2,10 @@
 
 // console.log(fruit[2]);
 
-var fruits = ['mango', 'apple', ['lychee', 'watermelon'], 'lemon'];
+// var fruits = ['mango', 'apple', ['lychee', 'watermelon'], 'lemon'];
 
-console.log(fruits[2][1]);
+// console.log(fruits[2][1]);
+
+var item = ['mango', 'apple', [1, 4, 7, 0], ['lychee', 'watermelon'], 'lemon'];
+
+console.log(item[2][3]);
