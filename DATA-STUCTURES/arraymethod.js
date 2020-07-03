2b101d7597de727dad61e8a3e8f63f01d5b8d81b
Matthew He <matthew.he.au@gmail.com>
2020-07-03 19:54:19 +1000

    Array print all of them with loop through

--- DATA-STUCTURES/arraymethod.js
+++ DATA-STUCTURES/arraymethod.js
@@ -7,7 +7,11 @@
 // console.log('I love eating ' + recipe.ingredients[3]);
 
 var ingredient = ['flour', 'eggs', 'milk', 'sugar'];
-console.log(ingredient[0]);
-console.log(ingredient[1]);
-console.log(ingredient[2]);
-console.log(ingredient[3]);
+// console.log(ingredient[0]);
+// console.log(ingredient[1]);
+// console.log(ingredient[2]);
+// console.log(ingredient[3]);
+
+for (let i = 0; i < ingredient.length; i++) {
+    console.log(ingredient[i]);
+}
