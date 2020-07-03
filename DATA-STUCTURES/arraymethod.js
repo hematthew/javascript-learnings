d3d6782adf9231d1c5f98ef107e6a7509b49b3f7
Matthew He <matthew.he.au@gmail.com>
2020-07-03 19:42:01 +1000

    Array print al the ingredients

--- DATA-STUCTURES/arraymethod.js
+++ DATA-STUCTURES/arraymethod.js
@@ -1,7 +1,13 @@
-var recipe = {
-    title: 'Titanic',
-    people: 2,
-    ingredients: ['flour', 'eggs', 'milk', 'sugar']
-}
-// console.log(recipe.ingredients[1]);
-console.log('I love eating ' + recipe.ingredients[3]);
+// var recipe = {
+//     title: 'Titanic',
+//     people: 2,
+//     ingredients: ['flour', 'eggs', 'milk', 'sugar']
+// }
+// // console.log(recipe.ingredients[1]);
+// console.log('I love eating ' + recipe.ingredients[3]);
+
+var ingredient = ['flour', 'eggs', 'milk', 'sugar'];
+console.log(ingredient[0]);
+console.log(ingredient[1]);
+console.log(ingredient[2]);
+console.log(ingredient[3]);
